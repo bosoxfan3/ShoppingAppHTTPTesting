184882de6a1704292c6e0d71a616365fb11ed880
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-12-01 21:21:10 -0800

    all tests passing

--- test/test-for-recipes.js
+++ test/test-for-recipes.js
@@ -1,35 +1,20 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-let {app} = require('../server');
+let {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
 
 chai.use(chaiHttp);
 
-let server;
-
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    })
-    .on('error', err => {
-      reject(err);
-    });
-  });
-}
-
-//... closeServer defined here
-
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
-
 describe('Recipes', function() {
-    it('should list items on GET', function() {
+	before(function() {
+		return runServer();
+	});
+	after(function() {
+		return closeServer();
+	});
+  it('should list items on GET', function() {
     return chai.request(app)
       .get('/recipes')
       .then(function(res) {
@@ -48,26 +33,23 @@ describe('Recipes', function() {
         });
       });
   });
-});
-
-it('should add an item on POST', function() {
-  const newItem = {name: 'KFC', ingredients: ['chicken', '11 secret herbs and spices']};
-  return chai.request(app)
-    .post('/recipes')
-    .send(newItem)
-    .then(function(res) {
-      res.should.have.status(201);
-      res.should.be.json;
-      res.body.should.be.a('object');
-      res.body.should.include.keys('id', 'name', 'ingredients');
-      res.body.id.should.not.be.null;
-      // response should be deep equal to `newItem` from above if we assign
-      // `id` to it from `res.body.id`
-      res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
-    });
-});
-
-it('should update items on PUT', function() {
+	it('should add an item on POST', function() {
+		const newItem = {name: 'KFC', ingredients: ['chicken', '11 secret herbs and spices']};
+			return chai.request(app)
+			.post('/recipes')
+			.send(newItem)
+			.then(function(res) {
+			res.should.have.status(201);
+			res.should.be.json;
+			res.body.should.be.a('object');
+			res.body.should.include.keys('id', 'name', 'ingredients');
+			res.body.id.should.not.be.null;
+			// response should be deep equal to `newItem` from above if we assign
+			// `id` to it from `res.body.id`
+			res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+		});
+	});
+	it('should update items on PUT', function() {
     const updateData = {
       name: 'foo',
       ingredients: ['bacon', 'lettuce', 'tomato']
@@ -88,8 +70,7 @@ it('should update items on PUT', function() {
         res.body.should.deep.equal(updateData);
       });
   });
-
-it('should delete items on DELETE', function() {
+	it('should delete items on DELETE', function() {
   return chai.request(app)
       // first have to get so we have an `id` of item
       // to delete
@@ -101,4 +82,5 @@ it('should delete items on DELETE', function() {
     .then(function(res) {
       res.should.have.status(204);
     });
+  });
 });
